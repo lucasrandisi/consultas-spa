c594c7040f056c5ed53a995ca6a49e1842296487
Nacho <ignaciocaramuto@gmail.com>
2022-04-29 10:49:45 -0300

    importado modulos de material

--- src/app/horarios/horarios.module.ts
+++ src/app/horarios/horarios.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ListarHorariosComponent } from './components/listar-horarios/listar-horarios.component';
+import { HorariosRoutingModule } from './horarios-routing.module';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     ListarHorariosComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HorariosRoutingModule,
+    MatTableModule,
+    MatButtonModule
   ]
 })
 export class HorariosModule { }
