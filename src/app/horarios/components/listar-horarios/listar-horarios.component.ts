7c1920c6220d1ecaa6857bc74005d91d6a45d75a
Nacho <ignaciocaramuto@gmail.com>
2022-05-24 12:03:01 -0300

    update: captura de param y getHorario ahora con filtro

--- src/app/horarios/components/listar-horarios/listar-horarios.component.ts
+++ src/app/horarios/components/listar-horarios/listar-horarios.component.ts
@@ -3,6 +3,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { Horarios } from '../../horarios.entities';
 import { HorariosService } from '../../horarios.service';
 import { InscripcionComponent } from '../inscripcion/inscripcion.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-listar-horarios',
@@ -14,10 +15,19 @@ export class ListarHorariosComponent implements OnInit {
   displayedColumns: string[] = ['dia', 'hora', 'profesor', 'accion'];
   horarios: Horarios[] = [];
 
-  constructor(private horariosService: HorariosService, public dialog: MatDialog) { }
+  idMateria: number;
+
+  constructor(
+    private horariosService: HorariosService,
+    public dialog: MatDialog,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.horariosService.getHorarios().subscribe(datos => {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.idMateria = params['id'];
+    });
+    
+    this.horariosService.getHorarios(this.idMateria).subscribe(datos => {
       this.horarios = datos;
     });
   }
