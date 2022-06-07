aace780d9513a0553f7b6609a22dbb2041ca9636
Nacho <ignaciocaramuto@gmail.com>
2022-06-07 18:13:39 -0300

    componente materias-admin

---
+++ src/app/materias-admin/components/listado-materias/listado-materias.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-materias',
+  templateUrl: './listado-materias.component.html',
+  styleUrls: ['./listado-materias.component.scss']
+})
+export class ListadoMateriasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
