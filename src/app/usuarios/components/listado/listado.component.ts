b24e1a9f233f8d20964b6c0841ae879b77707942
Lucas Randisi <lucasrandisi@gmail.com>
2022-06-21 17:34:17 -0300

    Remove http client module from usuarios module

--- src/app/usuarios/components/listado/listado.component.ts
+++ src/app/usuarios/components/listado/listado.component.ts
@@ -1,48 +1,45 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 
-import { MatSort} from '@angular/material/sort';
+import { MatSort } from '@angular/material/sort';
 import { Usuario } from './usuarios';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from 'src/environments/environment';
 import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
-  selector: 'app-listado',
-  templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.scss']
+	selector: 'app-listado',
+	templateUrl: './listado.component.html',
+	styleUrls: ['./listado.component.scss']
 })
 export class ListadoComponent implements OnInit {
 
-  dataSource = new MatTableDataSource();
+	dataSource = new MatTableDataSource();
 
-  displayedColumns = ['usuario', 'agregar'];
+	displayedColumns = ['usuario', 'agregar'];
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort, {}) sort: MatSort;
-  
-  constructor(private http: HttpClient, private authService: AuthService) { }
+	@ViewChild(MatPaginator) paginator: MatPaginator;
+	@ViewChild(MatSort, {}) sort: MatSort;
 
-  
-     ngOnInit() {
-      this.getUsuarios();
-      console.log(this.authService.accessToken);
-  
-    }
+	constructor(private http: HttpClient, private authService: AuthService) { }
 
-    getUsuarios(){
-      this.http.get(`${environment.apiUrl}/users`,{
-        params: {
-        }
-    }).subscribe((response:any)=>{
-      console.log(response);
-      this.dataSource = response;
-    }
-  )
-  };
 
-  
+	ngOnInit() {
+		this.getUsuarios();
+		console.log(this.authService.accessToken);
+
+	}
+
+	getUsuarios() {
+		this.http.get(`${environment.apiUrl}/me`).subscribe((response: any) => {
+			console.log(response);
+			this.dataSource = response;
+		}
+		)
+	};
+
+
 
 }
--- src/app/usuarios/usuarios.module.ts
+++ src/app/usuarios/usuarios.module.ts
@@ -4,44 +4,40 @@ import { CommonModule } from '@angular/common';
 import { UsuariosRoutingModule } from './usuarios-routing.module';
 import { ListadoComponent } from './components/listado/listado.component';
 
-import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-
-
-import {MatInputModule} from '@angular/material/input';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { MatPaginatorModule } from '@angular/material/paginator';
 
-import {MatIcon, MatIconModule} from '@angular/material/icon';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { CrearUsuarioComponent } from './components/crear-usuario/crear-usuario.component';
 
 @NgModule({
-  declarations: [
-    ListadoComponent,
-    CrearUsuarioComponent
-  ],
-  imports: [
-    CommonModule,
-    UsuariosRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatInputModule,
-    MatTableModule,
-    MatTooltipModule,
-    MatTooltipModule,
-    MatSortModule,
-    MatPaginatorModule,
-    MatInputModule,
-    MatProgressSpinnerModule
-  ]
+	declarations: [
+		ListadoComponent,
+		CrearUsuarioComponent
+	],
+	imports: [
+		CommonModule,
+		UsuariosRoutingModule,
+		FormsModule,
+		ReactiveFormsModule,
+		MatButtonModule,
+		MatIconModule,
+		MatInputModule,
+		MatTableModule,
+		MatTooltipModule,
+		MatTooltipModule,
+		MatSortModule,
+		MatPaginatorModule,
+		MatInputModule,
+		MatProgressSpinnerModule
+	]
 })
 export class UsuariosModule { }
