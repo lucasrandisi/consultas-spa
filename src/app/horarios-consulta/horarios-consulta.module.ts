b819b01b6b84be1a496249c0f62fd4be6c65591f
Martin Pereyra <pereyramartinn@gmail.com>
2022-06-22 18:16:41 -0300

    esqueleto

--- src/app/horarios-consulta/horarios-consulta.module.ts
+++ src/app/horarios-consulta/horarios-consulta.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HorariosConsultaRoutingModule } from './horarios-consulta-routing.module';
+import { ListadoComponent } from './components/listado/listado.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListadoComponent
+  ],
   imports: [
     CommonModule,
     HorariosConsultaRoutingModule
