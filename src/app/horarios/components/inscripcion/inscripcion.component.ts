e9567ed84d9c93a461dd060a7f24ff805a389015
Nacho <ignaciocaramuto@gmail.com>
2022-06-23 23:33:25 -0300

    update: validacion y subscribe a servicio

--- src/app/horarios/components/inscripcion/inscripcion.component.ts
+++ src/app/horarios/components/inscripcion/inscripcion.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Inscripcion } from '../../horarios.entities';
+import { HorariosService } from '../../horarios.service';
 
 @Component({
   selector: 'app-inscripcion',
@@ -10,11 +12,26 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class InscripcionComponent implements OnInit {
 
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  id: number;
+  inscripcion: Inscripcion;
 
   constructor(public dialogRef: MatDialogRef<InscripcionComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: {id: number}) { }
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private horariosService: HorariosService) { this.id = data }
 
   ngOnInit(): void {
+  }
+
+  inscribirse(): void {
+    if (!this.emailFormControl.hasError('required') && !this.emailFormControl.hasError('email')) {
+      this.inscripcion = {
+        email: this.emailFormControl.value,
+        horario_consulta_id: this.id
+      };
+      this.horariosService.inscribirConsulta(this.inscripcion).subscribe(response => {
+        console.log(response);
+      });
+    }
   }
 
   closeDialog() {
