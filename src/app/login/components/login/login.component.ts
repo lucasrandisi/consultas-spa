b7b35538a83396e1fc4cf4ea84acb2074c8c30d5
Lucas Randisi <lrandisi@kodear.net>
2022-06-27 01:19:19 -0300

    Login error display

--- src/app/login/components/login/login.component.ts
+++ src/app/login/components/login/login.component.ts
@@ -9,31 +9,38 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  credentials: any = {}
+    credentials: any = {}
+        wrongCredentials = false;
 
 
+    constructor(private authService: AuthService,private router : Router) { }
 
-  constructor(private authService: AuthService,private router : Router) { }
+    ngOnInit(): void {
+    }
 
-  ngOnInit(): void {
-  }
-
-  login(){
-    this.authService.login(this.credentials).subscribe({
-      next: response => {
-        console.log(response.user);
-        if(response.user.rol.name == "Alumno"){
-          this.router.navigate(['/materias'])
+    login() {
+        if (!this.credentials.email || !this.credentials.password) {
+            return;
         }
-        if(response.user.rol.name == "Admin"){
-          this.router.navigate(['/usuarios']);
-        }
-        if(response.user.rol.name == "Profesor"){
-          this.router.navigate(['/horarios-consulta',response.user.id])
-        }
-      },
-      error: error => console.log(error)
-    })
+
+        this.authService.login(this.credentials).subscribe({
+        next: response => {
+            console.log(response.user);
+            if(response.user.rol.name == "Alumno"){
+            this.router.navigate(['/materias'])
+            }
+            if(response.user.rol.name == "Admin"){
+            this.router.navigate(['/usuarios']);
+            }
+            if(response.user.rol.name == "Profesor"){
+            this.router.navigate(['/horarios-consulta',response.user.id])
+            }
+        },
+            error: error => {
+                this.wrongCredentials = true;
+                console.log(error)
+            }
+        })
   }
 
 }
