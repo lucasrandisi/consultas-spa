5d1154a2fb90c8a7b8c0b8194eda2ca6e045fa60
Lucas Randisi <lrandisi@kodear.net>
2022-07-14 23:25:29 -0300

    Add paginator to user list

--- src/app/usuarios/components/listado/listado.component.ts
+++ src/app/usuarios/components/listado/listado.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 
 import { MatSort } from '@angular/material/sort';
 
@@ -14,29 +14,29 @@ import { AuthService } from 'src/app/shared/services/auth.service';
 	templateUrl: './listado.component.html',
 	styleUrls: ['./listado.component.scss']
 })
-export class ListadoComponent implements OnInit {
+export class ListadoComponent implements OnInit, AfterViewInit {
 
 	dataSource = new MatTableDataSource();
 
 	displayedColumns = ['id', 'rol_id', 'email', 'usuario'];
 
 	@ViewChild(MatPaginator) paginator: MatPaginator;
-	@ViewChild(MatSort, {}) sort: MatSort;
 
 	constructor(private http: HttpClient) { }
-
 
 	ngOnInit() {
 		this.getUsuarios();
 
-	}
+    }
+    
+    ngAfterViewInit(): void {
+        this.dataSource.paginator = this.paginator;
+    }
 
 	getUsuarios() {
-		this.http.get(`${environment.apiUrl}/users`).subscribe((response: any) => {
-			console.log(response);
-			this.dataSource = response.data;
-		}
-		)
+        this.http.get(`${environment.apiUrl}/users`).subscribe((response: any) => {
+            this.dataSource.data = response.data;
+        });
 	};
 
 
