8bcbcc6c18e60f162992ca736acb7aec0edc675d
Lucas Randisi <lrandisi@kodear.net>
2022-07-22 04:49:01 -0300

    Show message on not horarios de consulta available

--- src/app/horarios/components/inscripcion/inscripcion.component.ts
+++ src/app/horarios/components/inscripcion/inscripcion.component.ts
@@ -3,6 +3,7 @@ import { FormControl, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Inscripcion } from '../../horarios.entities';
 import { HorariosService } from '../../horarios.service';
+import { HorarioConsulta } from '../listar-horarios/listar-horarios.component';
 
 @Component({
   selector: 'app-inscripcion',
@@ -10,32 +11,36 @@ import { HorariosService } from '../../horarios.service';
   styleUrls: ['./inscripcion.component.scss']
 })
 export class InscripcionComponent implements OnInit {
+    emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+    horarioConsulta: HorarioConsulta;
+    inscripcion: Inscripcion;
 
-  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  id: number;
-  inscripcion: Inscripcion;
-
-  constructor(public dialogRef: MatDialogRef<InscripcionComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any,
-    private horariosService: HorariosService) { this.id = data }
-
-  ngOnInit(): void {
-  }
-
-  inscribirse(): void {
-    if (!this.emailFormControl.hasError('required') && !this.emailFormControl.hasError('email')) {
-      this.inscripcion = {
-        email: this.emailFormControl.value,
-        horario_consulta_id: this.id
-      };
-      this.horariosService.inscribirConsulta(this.inscripcion).subscribe(response => {
-        this.dialogRef.close(response);
-      });
+    constructor(
+        public dialogRef: MatDialogRef<InscripcionComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: any,
+        private horariosService: HorariosService
+    ) {
+        this.horarioConsulta = data.horarioConsulta;
     }
-  }
 
-  closeDialog() {
-    this.dialogRef.close();
-  }
+    ngOnInit(): void {
+    }
+
+    inscribirse(): void {
+        if (!this.emailFormControl.hasError('required') && !this.emailFormControl.hasError('email')) {
+            this.inscripcion = {
+                email: this.emailFormControl.value,
+                horario_consulta_id: this.horarioConsulta.id
+            };
+                
+            this.horariosService.inscribirConsulta(this.inscripcion).subscribe(response => {
+                this.dialogRef.close(response);
+            });
+        }
+    }
+
+    closeDialog() {
+        this.dialogRef.close();
+    }
 
 }
--- src/app/horarios/components/listar-horarios/listar-horarios.component.ts
+++ src/app/horarios/components/listar-horarios/listar-horarios.component.ts
@@ -5,6 +5,13 @@ import { HorariosService } from '../../horarios.service';
 import { InscripcionComponent } from '../inscripcion/inscripcion.component';
 import { ActivatedRoute } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+
+export type HorarioConsulta = {
+    id: number,
+    profesor: any,
+    materia: any,
+};
 
 @Component({
   selector: 'app-listar-horarios',
@@ -12,45 +19,43 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./listar-horarios.component.scss']
 })
 export class ListarHorariosComponent implements OnInit {
+    displayedColumns: string[] = ['dia', 'hora', 'profesor', 'accion'];
+    horarios$: Observable<Horarios[]>;
+    materiaId: number;
+    durationInSeconds = 3;
 
-  displayedColumns: string[] = ['dia', 'hora', 'profesor', 'accion'];
-  horarios: Horarios[] = [];
+    constructor(
+        private horariosService: HorariosService,
+        public dialog: MatDialog,
+        private activatedRoute: ActivatedRoute,
+        private _snackBar: MatSnackBar) { }
 
-  idMateria: number;
-
-  durationInSeconds = 3;
-
-  constructor(
-    private horariosService: HorariosService,
-    public dialog: MatDialog,
-    private activatedRoute: ActivatedRoute,
-    private _snackBar: MatSnackBar) { }
-
-  ngOnInit(): void {
-    this.activatedRoute.queryParams.subscribe(params => {
-      this.idMateria = params['id'];
-    });
-    
-    this.horariosService.getHorarios(this.idMateria).subscribe(datos => {
-      this.horarios = datos;
-    });
-  }
-
-  openInscripcionDialog(id: number): void {
-    let dialogRef = this.dialog.open(InscripcionComponent, {
-      height: '260px',
-      width: '500px',
-      data: id
-    });
-    dialogRef.afterClosed().subscribe(response => {
-      if (response) {
-        this._snackBar.open('Inscripto a horario de consulta', 'Cerrar', {
-          duration: 3000,
-          verticalPosition: 'top',
-          horizontalPosition: 'end',
-          panelClass: ['success']
+    ngOnInit(): void {
+        this.activatedRoute.queryParams.subscribe(params => {
+            this.materiaId = params['id'];
         });
-      }
-    })
-  }
+        
+        this.horarios$ = this.horariosService.getHorarios(this.materiaId);
+    }
+
+    openInscripcionDialog(horarioConsulta: HorarioConsulta): void {
+        let dialogRef = this.dialog.open(InscripcionComponent, {
+            height: '260px',
+            width: '500px',
+            data: {
+                horarioConsulta: horarioConsulta
+            }
+        });
+
+        dialogRef.afterClosed().subscribe(response => {
+            if (response) {
+                this._snackBar.open('Inscripto a horario de consulta', 'Cerrar', {
+                    duration: 3000,
+                    verticalPosition: 'bottom',
+                    horizontalPosition: 'end',
+                    panelClass: ['success']
+                });
+            }
+        });
+    }
 }
